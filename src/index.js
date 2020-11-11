f41110250f61f9c90463607adab82a050a3058d4
Anton <eisentier.anton@gmail.com>
2020-11-11 19:13:38 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './sass/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/index';
+
+ReactDOM.render(<App />, document.querySelector('.area'));
